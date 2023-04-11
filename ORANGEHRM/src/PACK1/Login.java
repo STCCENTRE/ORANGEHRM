2e346a8a2303e231f0b0a58f92fa1e570f1852d0
arya <aryabr99@gmail.com>
2023-04-11 12:10:58 +0530

    new lines added

--- ORANGEHRM/src/PACK1/Login.java
+++ ORANGEHRM/src/PACK1/Login.java
@@ -1,5 +1,24 @@
 package PACK1;
 
 public class Login {
+	WebDriver driver;
 
+	@Test
+	public void Test()throws InterruptedException{
+
+
+	System.out.println("Launching Orange HRM");
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\Ajeesh\\Downloads\\selenium\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	driver=new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	driver.get("http://localhost/orangehrm/orangehrm-5.3/web/index.php/auth/login");
+
+	driver.findElement(By.name("username")).sendKeys("Admin");//username
+	driver.findElement(By.name("password")).sendKeys("Stc@1234");//password
+	driver.findElement(By.xpath("//button[@type='submit']")).click();//login
+
+
+}
 }
