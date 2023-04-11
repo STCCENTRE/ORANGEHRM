46fbcdd2f8eeec80036d537d779d2d0ce28070f2
malavika <malavikasasidhara@gmail.com>
2023-04-11 12:22:41 +0530

    new lines added

--- ORANGEHRM/src/PACK1/Login.java
+++ ORANGEHRM/src/PACK1/Login.java
@@ -1,5 +1,21 @@
 package PACK1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Login {
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\USER\\Desktop\\Selenum Web Driver\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+	
+	driver.get("http://localhost/orangehrm");
+	driver.findElement(By.name("username")).sendKeys("Admin");
+	driver.findElement(By.name("password")).sendKeys("Stc@1234");
+	
+	driver.findElement(By.cssSelector(".oxd-button")).click();
+	
 
 }
