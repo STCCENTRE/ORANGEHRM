cc60eb95165048f3f2a8e48915a93cc15f694c50
priyakurup <abc@gmail.com>
2023-08-25 13:16:09 +0530

    commited

--- ORANGEHRM/src/PACK1/Login.java
+++ ORANGEHRM/src/PACK1/Login.java
@@ -1,5 +1,24 @@
 package PACK1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 public class Login {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Dell\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost/orangehrm/orangehrm-5.5/web/index.php/dashboard/index");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("Stcpattom@1234");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).submit();
+	}
+	
+	
 
 }
