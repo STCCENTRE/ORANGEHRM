ac315d5b303c6ef99b020e7a86764c683bc05617
Arjun <abcd@gmail.com>
2023-08-25 13:09:29 +0530

    created orangehrm project

--- ORANGEHRM/src/PACK1/Login.java
+++ ORANGEHRM/src/PACK1/Login.java
@@ -1,5 +1,24 @@
 package PACK1;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class Login {
+	@Test
+	public void loginorangehrm() {
+		System.setProperty("webdriver.gecko.driver","D:\\Software Testing\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://localhost/orangehrm/orangehrm-5.5/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("Stcpattom@123");
+		driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+	}
+	
 
 }
