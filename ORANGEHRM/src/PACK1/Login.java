1877225db81f4a627a6d7562f5d6f6000e973b24
Jerin <mjerin003@gmail.com>
2023-11-29 15:18:57 +0530

    complet

--- ORANGEHRM/src/PACK1/Login.java
+++ ORANGEHRM/src/PACK1/Login.java
@@ -1,8 +1,18 @@
 package PACK1;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	import org.testng.annotations.Test;
 
-public class Login {
-	public static void main(String[] args)
-	{
-	System.out.println("Hello");
-}
+	public class branchjerin {
+	@Test
+	public void facebooktest() {
+	System.setProperty("webdriver.gecko.driver","C:\\Users\\Poby\\Downloads\\selenium\\New folder\\firefox\\geckodriver.exe");
+	WebDriver driver =new FirefoxDriver();
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.name("email")).sendKeys("Jerin");
+	driver.findElement(By.name("pass")).sendKeys("password3456");
+	driver.findElement(By.xpath("//html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).click();
+
+	}	
 }
